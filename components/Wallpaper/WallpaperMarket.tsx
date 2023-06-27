f5abe7eb49b8780d1cdf44594a01590600c7c1ce
Developer27149 <564151947@qq.com>
2023-06-27 23:01:44 +0800

    feat: add like and all wallpaper filter

--- components/Wallpaper/WallpaperMarket.tsx
+++ components/Wallpaper/WallpaperMarket.tsx
@@ -19,9 +19,10 @@ export default function () {
     setWallpaperList,
     setLikeList,
     likeList,
-    updateLikeList
+    updateLikeList,
+    type,
+    onSwitchType
   } = useImageList()
-  const [type, setType] = useState("like")
   const [, setLoading] = useAtom(isLoadingWallpaperStore)
 
   const containerRef = useRef<HTMLDivElement>(null)
@@ -71,67 +72,85 @@ export default function () {
 
   return (
     <div
-      className="w-[1200px]  max-h-[65vh] max-w-[90vw]  p-4 bg-gray-50 rounded-md fixed left-[50%] top-[50%] transform -translate-x-[50%] -translate-y-[50%] overflow-y-scroll"
+      className="w-[1200px] min-h-[520px] max-h-[65vh] max-w-[90vw]  p-4 bg-gray-50 rounded-md fixed left-[50%] top-[50%] transform -translate-x-[50%] -translate-y-[50%] overflow-y-scroll"
       onContextMenu={onRightClick}
       ref={containerRef}>
       {/* tabs */}
-      <div className="mb-4 p-1 px-2 grid grid-cols-2 gap-2 bg-gray-200 text-gray-400 rounded-sm max-w-max items-center relative">
-        <span className="z-10">全部</span>
-        <span className="z-10">喜欢</span>
+      <div className="mb-4 p-1 px-2 grid grid-cols-2 gap-2 bg-gray-200 text-gray-400 rounded-sm max-w-max items-center relative text-center text-[13px]">
+        <span className="z-10 pt-[1px]" onClick={() => onSwitchType("all")}>
+          全部
+        </span>
+        <span className="z-10 pt-[1px]" onClick={() => onSwitchType("like")}>
+          喜欢
+        </span>
         <span
           className={clsx(
-            "absolute left-[4px] w-[36px] bg-green-50 h-[22px] transition-all"
+            "absolute left-[4px] bottom-[3px] w-[34px] bg-green-50 h-[22px] transition-all",
+            {
+              "transform translate-x-0": type === "all",
+              "transform translate-x-[34px]": type === "like"
+            }
           )}></span>
       </div>
-      <div className="grid grid-cols-3 gap-4">
-        {wallpaperList.map(({ urlbase, copyright }) => {
-          return (
-            <div
-              key={urlbase}
-              className={clsx("relative", {
-                "animate-pulse bg-gray-100": !loadRecordMap[urlbase]
-              })}>
-              <img
-                src={generatePreviewWallpaperUrl(urlbase)}
-                onLoad={() => onLoadSuccess(urlbase)}
-                alt=""
-                className={clsx(
-                  "w-full h-[214px] object-cover p-1 bg-gray-50 rounded-t-md",
-                  {
-                    "opacity-0": !loadRecordMap[urlbase]
-                  }
-                )}
-              />
+      {wallpaperList.length === 0 ? (
+        <div className="bg-gray-50 text-gray-400 h-[400px] flex justify-center items-center">
+          这儿什么都没有...
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {wallpaperList.map(({ urlbase, copyright }) => {
+            return (
               <div
-                className={clsx(
-                  "absolute bottom-0 left-0 right-0 p-2 bg-gray-900 bg-opacity-50 rounded-md",
-                  {
-                    "opacity-0": !loadRecordMap[urlbase]
-                  }
-                )}>
-                <div className="flex justify-between items-center">
-                  <div className="text-gray-50 text-sm max-w-[80%] truncate">
-                    {copyright}
-                  </div>
-                  <div className="text-gray-50 text-sm">
-                    {likeList.current?.includes(urlbase) ? (
-                      <FcLike onClick={() => onReverseLikeWallpaper(urlbase)} />
-                    ) : (
-                      <FcLikePlaceholder
-                        className="opacity-50"
-                        onClick={() => onReverseLikeWallpaper(urlbase)}
-                      />
-                    )}
+                key={urlbase}
+                className={clsx("relative", {
+                  "animate-pulse bg-gray-100": !loadRecordMap[urlbase]
+                })}>
+                <img
+                  src={generatePreviewWallpaperUrl(urlbase)}
+                  onLoad={() => onLoadSuccess(urlbase)}
+                  alt=""
+                  className={clsx(
+                    "w-full h-[214px] object-cover p-1 bg-gray-50 rounded-t-md",
+                    {
+                      "opacity-0": !loadRecordMap[urlbase]
+                    }
+                  )}
+                />
+                <div
+                  className={clsx(
+                    "absolute bottom-0 left-0 right-0 p-2 bg-gray-900 bg-opacity-50 rounded-md",
+                    {
+                      "opacity-0": !loadRecordMap[urlbase]
+                    }
+                  )}>
+                  <div className="flex justify-between items-center">
+                    <div className="text-gray-50 text-sm max-w-[80%] truncate">
+                      {copyright}
+                    </div>
+                    <div className="text-gray-50 text-sm">
+                      {likeList.current?.includes(urlbase) ? (
+                        <FcLike
+                          onClick={() => onReverseLikeWallpaper(urlbase)}
+                        />
+                      ) : (
+                        <FcLikePlaceholder
+                          className="opacity-50"
+                          onClick={() => onReverseLikeWallpaper(urlbase)}
+                        />
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
+            )
+          })}
+          {hadMoreWallpaper ?? (
+            <div className="h-[214px] flex justify-center items-center">
+              more
             </div>
-          )
-        })}
-        {hadMoreWallpaper ?? (
-          <div className="h-[214px] flex justify-center items-center">more</div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
--- hooks/useImageList.ts
+++ hooks/useImageList.ts
@@ -5,15 +5,21 @@ import { onGetCurrentWallpaper } from "~utils/wallpaper";
 
 export default function useImageList() {
   const imgList = useRef([] as IWeekImage[])
+  const [type, setType] = useState("all")
+
   const limit = useRef(7)
   const [wallpaperList, setWallpaperList] = useState([] as IWeekImage[])
   const [hadMoreWallpaper, setHadMoreWallpaper] = useState(true)
   const likeList = useRef([] as string[])
   const loadMore = useCallback(() => {
-    if (limit.current < imgList.current.length) {
+    const targetList = imgList.current.filter(item => {
+      if (type === 'all') return true
+      return likeList.current.includes(item.urlbase)
+    })
+    if (limit.current < targetList.length) {
       limit.current += 7
-      setHadMoreWallpaper(limit.current + 7 < imgList.current.length)
-      setWallpaperList(imgList.current.slice(0, limit.current))
+      setHadMoreWallpaper(limit.current + 7 < targetList.length)
+      setWallpaperList(targetList.slice(0, limit.current))
     } else {
       setHadMoreWallpaper(false)
     }
@@ -29,12 +35,26 @@ export default function useImageList() {
     })
   }
 
+
+  const onSwitchType = (newType: 'all' | 'like') => {
+    setType(newType)
+    const filterList = imgList.current.filter(item => {
+      if (newType === 'all') return true
+      return likeList.current.includes(item.urlbase)
+    })
+    limit.current = 7
+    setHadMoreWallpaper(limit.current + 7 < filterList.length)
+    setWallpaperList(filterList.slice(0, limit.current))
+  }
+
+
   useEffect(() => {
     // get image list from storage
     const init = async () => {
       const result = await chrome.storage.local.get(EStorageKey.imageList)
       const { url } = await onGetCurrentWallpaper()
       const imageListData = (result[EStorageKey.imageList] as IWeekImage[]).filter(item => !url.includes(item.urlbase))
+      // const imageListData = []
       imgList.current = imageListData
       setWallpaperList(imageListData.slice(0, limit.current))
       const syncResult = await chrome.storage.sync.get(EStorageKey.likeList)
@@ -48,6 +68,7 @@ export default function useImageList() {
 
   return {
     loadMore, wallpaperList, hadMoreWallpaper, setWallpaperList, likeList, setLikeList,
-    updateLikeList
+    updateLikeList,
+    type, onSwitchType
   }
 }
